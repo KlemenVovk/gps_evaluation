caa48eb6bb06a51a8540e6c2ea28594f94574a90
KlemenVovk <klemen.vovk@protonmail.com>
2024-03-15 15:41:16 +0100

    Add high density intervals.

--- bayesian.r
+++ bayesian.r
@@ -1,17 +1,19 @@
-# libraries --------------------------------------------------------------------
 library(cmdstanr)
 library(bayesplot)
 library(mcmcse)
 library(posterior)
 
+# csv of points
 csv <- 'p1'
-
 df <- read.csv(paste0(csv, ".csv"))
+
+# compile the stan model
 model <- cmdstan_model("model.stan")
 
+# fit a bivariate normal distribution using stan (MCMC)
 stan_data <- list(
   n = nrow(df),
-  x = matrix(c(df$Distance, df$Angle), nrow = nrow(df), ncol = 2)
+  x = matrix(c(df$dist, df$angle), nrow = nrow(df), ncol = 2)
 )
 
 fit <- model$sample(
@@ -20,9 +22,11 @@ fit <- model$sample(
   parallel_chains = 4
 )
 
+# diagnostics
 mcmc_trace(fit$draws())
 fit$summary()
 
+# save the draws
 results <- as_draws_df(fit$draws())
 results <- results[,c(2,3,4,5,6,7)]
 colnames(results) <- c("mu_dist", "mu_angle", "sigma11", "sigma21", "sigma12", "sigma22")
